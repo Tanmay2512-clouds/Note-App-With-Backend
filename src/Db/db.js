2867e5689b327f9073d96d78fe53ae47ce2c7c8a
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-03 19:33:00 +0530

    Set up server and database connection for the note app backend

---
+++ src/Db/db.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose")
+
+async function connectDB() {
+    await mongoose.connect("mongodb+srv://Tanmay-2:Z31gScjHnBCaUvc8@backend-dev.tdwsxlj.mongodb.net/Project-1")
+    console.log("Connected to DB")
+}

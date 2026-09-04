8dd90a4b51a295beb9526cde788f8f2a9148268a
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-04 21:23:01 +0530

    Fix upload functionality by ensuring file buffer is processed in create-post endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-
+const uploadFile = require('./Service/storage.service')
 
 const app = express();
 app.use(express.json())
@@ -10,6 +10,8 @@ const upload = multer({storage:multer.memoryStorage()})
 app.post('/create-post',upload.single("image"),async(req,res)=>{
     console.log(req.body);
     console.log(req.file);
+
+    const result = await uploadFile(req.file.buffer)
 })
 
 module.exports = app;

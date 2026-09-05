0edeb5eef4002114cb02b7252b47701c0bc09e85
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-05 15:48:54 +0530

    feat: update post submission logic to navigate to feed on success

--- Frontend/src/Pages/CreatePost.jsx
+++ Frontend/src/Pages/CreatePost.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
 
 const CreatePost = () => {
+
+  const navigate = useNavigate()
 
   const handleSubmit = async (e)=>{
 
     const formData = new FormData(e.target)
     axios.post("http://localhost:3000/create-post",formData)
     .then((res)=>{
-      alert("Post Created Successfully")
-      e.target.reset()
+      navigate("/feed")
     })
     .catch((err)=>{
       console.log(err)

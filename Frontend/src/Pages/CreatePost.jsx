e7d4a8f35427d72c0398e6ee1962116b9a0da5cb
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-05 15:38:52 +0530

    feat: implement post creation functionality in CreatePost component

--- Frontend/src/Pages/CreatePost.jsx
+++ Frontend/src/Pages/CreatePost.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import axios from 'axios'
 
 const CreatePost = () => {
+
+  const handleSubmit = async (e)=>{
+
+    const formData = new FormData(e.target)
+    axios.post("http://localhost:3000/create-post",formData)
+    .then((res)=>{
+      alert("Post Created Successfully")
+      e.target.reset()
+    })
+    .catch((err)=>{
+      console.log(err)
+      alert("Error Creating Posts")
+    })
+    e.preventDefault()
+  }
   return (
     <section className='create-post-section'>
         <h1>Create Post</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type="file" name='image' accept='image/*' />
             <input type="text" name='caption' required placeholder='Enter Caption'/>
             <button type='submit'>Submit</button>

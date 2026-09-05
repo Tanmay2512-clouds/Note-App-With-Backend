0e8bb2f40ee48a10c00720197576f28dd109fecd
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-05 12:16:08 +0530

    feat: add Feed component and update routing in App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import CreatePost from './Pages/CreatePost'
+import Feed from './Pages/Feed'
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/create-post' element={<CreatePost/>} />
-        <Route path='/about' element={<h1>About us</h1>} />
+        <Route path='/feed' element={<Feed/>} />
       </Routes>
     </Router>
   )
---
+++ Frontend/src/Pages/Feed.jsx
@@ -0,0 +1,28 @@
+import React,{useState} from 'react'
+
+const Feed = () => {
+    const [posts,setPosts] = useState([{
+        _id:"1",
+        image:"https://images.unsplash.com/photo-1773332585754-f1436987743b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        caption:"Beautiful Sceneary"
+    }])
+
+    return (
+        <section className='feed-section'>
+            {
+                posts.length>0 ?(
+                    posts.map((post)=>(
+                        <div key={post._id} className='post-card'>
+                            <img src={post.image} alt={post.caption} srcset="" />
+                            <p>{post.caption}</p>
+                        </div>
+                    ))
+                ):(
+                    <h1>Post Not Available</h1>
+                )
+            }
+        </section>
+    )
+}
+
+export default Feed
